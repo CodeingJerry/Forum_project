eec1e71fe0af40905125b7126c17bc0d47301726
lijunchao16 <895277169@qq.com>
2016-04-08 21:30:16 +0800

    done

--- article/views.py
+++ article/views.py
@@ -6,5 +6,5 @@ from models import Article
 def article_list(request,block_id):
     block_id = int(block_id)
     block = Block.objects.get(id=block_id)
-    articles = Article.objects.filter(block=block).order_by(".last_update_timestamp")
+    articles = Article.objects.filter(block=block).order_by("-last_update_timestamp")
     return render_to_response("article_list.html",{"articles":articles,"block":block})
