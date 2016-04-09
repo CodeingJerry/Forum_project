225689b5a19e7523ab5f0851ad10b8c5749db58b
lijunchao16 <895277169@qq.com>
2016-04-09 19:38:11 +0800

    done0409

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from article.views import article_list
+from article.views import article_list,create_article,ariticle_detail
 
 urlpatterns = [
     url(r'^lists/(?P<block_id>\d+)', article_list, name="article_list"),
+    url(r'^create/(?P<block_id>\d+)', create_article, name="create_article"),
+    url(r'^detail/(?P<article_id>\d+)', ariticle_detail, name="ariticle_detail"),
 ]
