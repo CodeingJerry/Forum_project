b798a18f55a0008ccd121022ae09e9faca960ce1
lijunchao16 <895277169@qq.com>
2016-04-09 10:11:03 +0800

    0409

--- article/urls.py
+++ article/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+from article.views import article_list
 
 urlpatterns = [
-    url(r'^lists/(?P<block_id>\d+)', "article.views.article_list", name="article_list"),
+    url(r'^lists/(?P<block_id>\d+)', article_list, name="article_list"),
 ]
